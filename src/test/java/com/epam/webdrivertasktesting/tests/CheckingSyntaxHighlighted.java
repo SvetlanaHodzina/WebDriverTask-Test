ee6ed48157059809d743661e4c60a5245d0e1a91
Sviatlana Hodzina <72805375+SvetlanaHodzina@users.noreply.github.com>
2021-12-04 22:07:30 +0300

    src uploaded

---
+++ src/test/java/com/epam/webdrivertasktesting/tests/CheckingSyntaxHighlighted.java
@@ -0,0 +1,25 @@
+package com.epam.webdrivertasktesting.tests;
+
+import com.epam.webdrivertasktesting.model.NewPaste;
+import com.epam.webdrivertasktesting.page.NewPasteSavedPage;
+import com.epam.webdrivertasktesting.page.PastebinPage;
+import com.epam.webdrivertasktesting.service.ComplexPaste;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+public class CheckingSyntaxHighlighted extends RequiredConditions {
+    @Test(description = "Verifies matching of Syntax Highlighted " +
+                        "to the corresponding value in .properties resource file")
+    public void verifySyntaxHighlighted(){
+        NewPaste newPaste = ComplexPaste.withCredentialsFromProperty();
+
+        new PastebinPage()
+                .openPage()
+                .createNewPaste(newPaste);
+        String syntaxHighlighted = new NewPasteSavedPage().getSyntaxHighlighted(newPaste.getSyntaxHighLighting());
+
+        assertThat(syntaxHighlighted, containsString(newPaste.getSyntaxHighLighting()));
+    }
+}
